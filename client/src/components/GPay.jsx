5a549669ce8f943c35c85c65df53aea48a13b6b8
ombayus <>
2021-08-19 21:56:02 +0300

    Metamask setupNetwork

--- client/src/components/GPay.jsx
+++ client/src/components/GPay.jsx
@@ -1,47 +1,54 @@
 import React from "react"
 import GooglePayButton from "@google-pay/button-react"
+import {useHistory} from "react-router-dom"
 
 const GPay = ()=>{
+
+    const history = useHistory()
+
     return(
-        <div className="d-flex justify-content-center">
-            <GooglePayButton
-                environment='TEST'
-                paymentRequest={{
-                    apiVersion: 2,
-                    apiVersionMinor: 0,
-                    allowedPaymentMethods: [
-                    {
-                        type: 'CARD',
-                        parameters: {
-                        allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
-                        allowedCardNetworks: ['MASTERCARD', 'VISA'],
+        <>
+            <div className="back" onClick={()=>history.push("/")}><span>{'<'}</span></div>
+            <div className="d-flex justify-content-center">
+                <GooglePayButton
+                    environment='TEST'
+                    paymentRequest={{
+                        apiVersion: 2,
+                        apiVersionMinor: 0,
+                        allowedPaymentMethods: [
+                        {
+                            type: 'CARD',
+                            parameters: {
+                            allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+                            allowedCardNetworks: ['MASTERCARD', 'VISA'],
+                            },
+                            tokenizationSpecification: {
+                            type: 'PAYMENT_GATEWAY',
+                            parameters: {
+                                gateway: 'example',
+                                gatewayMerchantId: 'exampleGatewayMerchantId'
+                            },
+                            },
                         },
-                        tokenizationSpecification: {
-                        type: 'PAYMENT_GATEWAY',
-                        parameters: {
-                            gateway: 'example',
-                            gatewayMerchantId: 'exampleGatewayMerchantId'
+                        ],
+                        merchantInfo: {
+                        merchantId: 'BCR2DN6T26J2XTZO',
+                        merchantName: 'OmBayus',
                         },
+                        transactionInfo: {
+                            totalPriceStatus: 'FINAL',
+                            totalPriceLabel: 'Total',
+                            totalPrice: '100.00',
+                            currencyCode: 'USD',
+                            countryCode: 'US',
                         },
-                    },
-                    ],
-                    merchantInfo: {
-                    merchantId: 'BCR2DN6T26J2XTZO',
-                    merchantName: 'OmBayus',
-                    },
-                    transactionInfo: {
-                        totalPriceStatus: 'FINAL',
-                        totalPriceLabel: 'Total',
-                        totalPrice: '100.00',
-                        currencyCode: 'USD',
-                        countryCode: 'US',
-                    },
-                }}
-                onLoadPaymentData={paymentRequest => {
-                    console.log('load payment data', paymentRequest.paymentMethodData);
-                }}
-            />
-        </div>
+                    }}
+                    onLoadPaymentData={paymentRequest => {
+                        console.log('load payment data', paymentRequest.paymentMethodData);
+                    }}
+                />
+            </div>
+        </>
     )
 }
 
--- client/src/components/MetaMask.jsx
+++ client/src/components/MetaMask.jsx
@@ -1,10 +1,45 @@
 import React, { useEffect, useState } from "react"
 import Web3 from "web3"
+import {useHistory} from "react-router-dom"
+
+const setupNetwork = async () => {
+    const provider = window.ethereum
+    if (provider) {
+      const chainId = parseInt("56", 10)
+      try {
+        await provider.request({
+          method: 'wallet_addEthereumChain',
+          params: [
+            {
+              chainId: `0x${chainId.toString(16)}`,
+              chainName: 'Binance Smart Chain Mainnet',
+              nativeCurrency: {
+                name: 'BNB',
+                symbol: 'bnb',
+                decimals: 18,
+              },
+              rpcUrls: ["https://bsc-dataseed1.ninicoin.io","https://bsc-dataseed1.defibit.io","https://bsc-dataseed.binance.org"],
+              blockExplorerUrls: ['https://bscscan.com'],
+            },
+          ],
+        })
+        return true
+      } catch (error) {
+        console.error('Failed to setup the network in Metamask:', error)
+        return false
+      }
+    } else {
+      console.error("Can't setup the BSC network on metamask because window.ethereum is undefined")
+      return false
+    }
+}
 
 const MetaMask = ()=>{
     const [amount,setAmount] = useState(0.01)
     const [web3,setWeb3] = useState(null)
     const [address,setAdress] = useState(null)
+
+    const history = useHistory()
 
     useEffect(()=>{
         var instance
@@ -27,6 +62,12 @@ const MetaMask = ()=>{
         setWeb3(instance)
     },[])
 
+    useEffect(()=>{
+        window.ethereum.on('accountsChanged', function (accounts) {
+            setAdress(accounts[0])
+          });
+    },[])
+
     const connect = async()=>{
         if(window.ethereum){
             await window.ethereum.enable()
@@ -38,6 +79,13 @@ const MetaMask = ()=>{
 
     const sendBNB = async()=>{
         const toAddress = "0xc3039A01507a7aAA375800f726F0c279aa106CaC"
+        const networkId = await web3.eth.net.getId();
+        console.log(networkId)
+        if(networkId !== 56){
+            await setupNetwork()
+            return
+        }
+        
         await web3.eth.sendTransaction({
             from: address,
             to:toAddress,
@@ -52,19 +100,22 @@ const MetaMask = ()=>{
 
 
     return(
-        <div className="d-flex justify-content-center text-center">
-            <div>
-                {address && <input type="number" step="0.01" className="my-3" onChange={e=>{
-                    if(e.target.value > 0){
-                        setAmount(e.target.value)}
-                    }
-                    
-                    } value={amount} />}
-                <br/>
-                {!address &&<button onClick={connect}>Connect Your MetaMask Wallet</button>}
-                {address && <button onClick={sendBNB}>Send BNB</button>}
+        <>
+            <div className="back" onClick={()=>history.push("/")}><span>{'<'}</span></div>
+            <div className="d-flex justify-content-center text-center">
+                <div>
+                    {address && <input type="number" step="0.01" className="my-3" onChange={e=>{
+                        if(e.target.value > 0){
+                            setAmount(e.target.value)}
+                        }
+                        
+                        } value={amount} />}
+                    <br/>
+                    {!address &&<button onClick={connect}>Connect Your MetaMask Wallet</button>}
+                    {address && <button onClick={sendBNB}>Send BNB</button>}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
