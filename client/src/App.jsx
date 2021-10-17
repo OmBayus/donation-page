1d93fe197322e44c0eaa1c777572c84a24bbfeb0
ombayus <>
2021-10-17 15:43:33 +0300

    ReactGa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
-import React from "react"
+import React, { useEffect } from "react"
 import {Switch,Route} from "react-router-dom";
+import ReactGA from 'react-ga';
 
 //components
 import Main from "./components/Main";
 import SelectPayment from "./components/SelectPayment";
 import GPay from "./components/GPay";
 import MetaMask from "./components/MetaMask";
-import CreditCard from "./components/CreditCard";
+import CreditCard from "./components/Shopier";
 
 const App = ()=>{
+
+    useEffect(()=>{
+        ReactGA.initialize('UA-000000-01');
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    },[])
     return(
         <Main>
             <Switch>
